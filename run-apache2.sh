42db30a3b0f4032f95775607cde5822d83a2814e
Dan Groleau <ddgroleau@gmail.com>
2022-06-12 15:12:01 -0600

    Updated dockerfile

--- run-apache2.sh
+++ run-apache2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo chmod u+x /etc/apache2/ports.conf
+chmod u+x /etc/apache2/ports.conf
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
 apache2-foreground "$@"
