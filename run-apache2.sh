78c9bb4eaaf68e3c6aa421d5178d59da12550db1
Dan Groleau <ddgroleau@gmail.com>
2022-06-12 15:18:30 -0600

    Updated dockerfile

--- run-apache2.sh
+++ run-apache2.sh
@@ -5,4 +5,5 @@ sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/apache2.conf
 sed -i "s/VirtualHost \*:80/VirtualHost \*:${PORT:-80}/g" /etc/apache2/sites-available/000-default.conf
 apache2-foreground "$@"
+apt-get install heroku
 heroku labs:enable --app=blog-fullstackdan runtime-new-layer-extract
